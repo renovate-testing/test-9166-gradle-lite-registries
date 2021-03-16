945d41d8a6d18432c260d83061f60062efc2eded
Tom Warner <twarner@sofi.org>
2021-03-16 09:38:24 -0600

    3 arg bug

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.4.30"
+}
+
+group = "com.sofi"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val jwtVersion: String by project
+
+    api("io.jsonwebtoken", "jjwt-api", jwtVersion)
+    api("com.fasterxml.jackson.core", "jackson-databind", "2.10.5")
+}
